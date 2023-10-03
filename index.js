de1ba70c7377983957ca4f51bb522a1c499e08ce
hie <hie2907>
2023-10-03 23:20:48 +0700

    final

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-const app = require("express")();
-var path = require("path");
-//view engine setup
-app.set("views", path.join(__dirname, "views")); //setting views directory for views.
-app.set("view engine", "hbs"); //setting view engine as handlebars
-app.get("/", (req, res) => {
-  let peopleList = getRandomList();
-  res.render("index", { people: peopleList }); //passing list of people to our index.hbs file.
+const http = require("http");
+const port = process.env.PORT || 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end("<h1>Hello World</h1>");
 });
-let getRandomList = () => {
-  let list = ["ada", "turing", "lovelace", "neumann", "gracehopper"];
-  let limit = Math.floor(Math.random() * (list.length - 1 - 0) + 0); //generating random
-
-  return list.slice(limit);
-};
-app.listen(5000);
+server.listen(port, () => {
+  console.log(`Server running at port ` + port);
+});
